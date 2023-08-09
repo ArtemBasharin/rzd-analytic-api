1400717a70c5c32fdd5cd570e76069bbf82b7c48
ArtemBasharin <temon-bash@yandex.ru>
2023-08-09 07:16:08 +0300

    some features has been added

--- src/controllers/violation-controller.js
+++ src/controllers/violation-controller.js
@@ -1,5 +1,5 @@
 const ObjectId = require("mongodb");
-const Violation = require("../models/violation");
+const Violations = require("../models/violation");
 const violationID = "ID отказа";
 
 const handleError = (res, error) => {
@@ -15,7 +15,7 @@ const getViolations = (req, res) => {
   let minY = req.query.fromYear + "-01-01";
   let maxY = req.query.toYear + "-12-31";
 
-  Violation.find({
+  Violations.find({
     $and: [
       {
         "Начало отказа": { $gte: new Date(minY) },
@@ -23,15 +23,15 @@ const getViolations = (req, res) => {
       { "Начало отказа": { $lte: new Date(maxY) } },
     ],
   })
-    .then((violations) => {
+    .then((result) => {
       // console.log(violations);
-      res.status(200).json(violations);
+      res.status(200).json(result);
     })
     .catch((err) => handleError(res, err));
 };
 
 const addViolation = (req, res) => {
-  const violation = new Violation(req.body);
+  const violation = new Violations(req.body);
   violation
     .save()
     .then((result) => {
@@ -41,10 +41,10 @@ const addViolation = (req, res) => {
 };
 
 const removeDups = (req, res) => {
-  Violation.find()
-    .then((violations) => {
-      violations.forEach(function (doc) {
-        Violation.deleteMany({
+  Violations.find()
+    .then((result) => {
+      result.forEach(function (doc) {
+        Violations.deleteMany({
           "ID отказа": doc["ID отказа"],
           _id: { $lt: doc._id },
         })
@@ -61,10 +61,10 @@ const removeDups = (req, res) => {
 const addBulkOfViolations = (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
 
-  Violation.find()
-    .then((violations) => {
-      violations.forEach(function (doc) {
-        Violation.deleteMany({
+  Violations.find()
+    .then((result) => {
+      result.forEach(function (doc) {
+        Violations.deleteMany({
           "ID отказа": doc["ID отказа"],
           _id: { $lt: doc._id },
         })
@@ -78,11 +78,10 @@ const addBulkOfViolations = (req, res) => {
     .catch((err) => handleError(res, err));
 
   let promises = [];
-
   if (Object.keys(req.body[0]).includes("Ответственный")) {
     console.log(req.body[0]);
     promises = req.body.map(function (el) {
-      return Violation.updateOne(
+      return Violations.updateOne(
         { "ID отказа": el["#"] },
         {
           "Виновное предприятие": el["Ответственный"].trim(),
@@ -94,7 +93,7 @@ const addBulkOfViolations = (req, res) => {
 
   if (Object.keys(req.body[0]).includes("Виновное предприятие")) {
     promises = req.body.map(function (el) {
-      return Violation.replaceOne(
+      return Violations.replaceOne(
         { "ID отказа": el["ID отказа"] },
         {
           "ID отказа": el["ID отказа"],
@@ -107,19 +106,19 @@ const addBulkOfViolations = (req, res) => {
           "Количество грузовых поездов(по месту)":
             el["Количество грузовых поездов(по месту)"],
           "Время грузовых поездов(по месту)":
-            el["Время грузовых поездов(по месту)"],
+            Math.round(el["Время грузовых поездов(по месту)"] / 6) / 10,
           "Количество пассажирских поездов(по месту)":
             el["Количество пассажирских поездов(по месту)"],
           "Время пассажирских поездов(по месту)":
-            el["Время пассажирских поездов(по месту)"],
+            Math.round(el["Время пассажирских поездов(по месту)"] / 6) / 10,
           "Количество пригородных поездов(по месту)":
             el["Количество пригородных поездов(по месту)"],
           "Время пригородных поездов(по месту)":
-            el["Время пригородных поездов(по месту)"],
+            Math.round(el["Время пригородных поездов(по месту)"] / 6) / 10,
           "Количество прочих поездов(по месту)":
             el["Количество прочих поездов(по месту)"],
           "Время прочих поездов(по месту)":
-            el["Время прочих поездов(по месту)"],
+            Math.round(el["Время прочих поездов(по месту)"] / 6) / 10,
         },
         { upsert: true }
       );
@@ -132,7 +131,7 @@ const addBulkOfViolations = (req, res) => {
 };
 
 const deleteViolations = (req, res) => {
-  Violation.deleteMany()
+  Violations.deleteMany()
     .then((result) => {
       res.status(200).json(result);
     })
@@ -140,7 +139,7 @@ const deleteViolations = (req, res) => {
 };
 
 const updateViolation = (req, res) => {
-  Violation.findByIdAndUpdate(req.params.id, req.body)
+  Violations.findByIdAndUpdate(req.params.id, req.body)
     .then((result) => {
       res.status(200).json(result);
     })
