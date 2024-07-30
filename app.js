09698998681db24971ee51102ad28773416ad7d1
ArtemBash <temon-bash@yandex.ru>
2024-07-30 15:58:42 +0700

    some settings for start

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongoose
 
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "https://rzd-analytic-api.vercel.app/",
     methods: "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS",
     preflightContinue: false,
     optionsSuccessStatus: 200,
